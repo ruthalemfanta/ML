a0811433895febe1fc037f9878e7b2cea3259dcf
ruthalemfanta <133218569+ruthalemfanta@users.noreply.github.com>
2024-08-16 22:25:26 +0300

    encoding catagorical data

--- data_processing.py
+++ data_processing.py
@@ -3,13 +3,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-dataset = pd.read_csv('Data.csv')
+#reading from a data set
+
+dataset = pd.read_csv('/Users/rutha./Downloads/1. Welcome to the course!/Machine Learning A-Z New/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Data.csv')
 x = dataset.iloc[:,:-1].values
 y = dataset.iloc[:,3].values
 
+#filling missing data 
 
 from sklearn.impute import SimpleImputer
 
 imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
 imputer = imputer.fit(x[:, 1:3])
 x[:, 1:3] = imputer.transform(x[:, 1:3])
+
+
+#catagorical data 
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_x = LabelEncoder()
+x[:,0] = labelencoder_x.fit_transform(x[:,0])
+onehotencoder = OneHotEncoder(categories=[0])
+x = onehotencoder.fit_transform(x).toarray()
